9074ebccf60cad87f7851a67dcc55b8ad4535930
lucasweyrich958 <157861096+lucasweyrich958@users.noreply.github.com>
2024-09-13 11:20:01 -0500

    Add files via upload

---
+++ Read Parquet.R
@@ -0,0 +1,6 @@
+#install.packages("httr")
+
+library(arrow)
+path = "C:\\Users\\f9tbqno\\Downloads\\sales.parquet" #Set path of Parquet File downloaded from Github repo: https://github.com/lucasweyrich958/DATA622/blob/main/sales.parquet
+
+sales = read_parquet(path)
