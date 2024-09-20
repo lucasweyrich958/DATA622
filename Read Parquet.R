64498d9010057e0746779efa6b1c3416d84d60a3
lucasweyrich958 <157861096+lucasweyrich958@users.noreply.github.com>
2024-09-20 10:45:24 -0500

    Delete Read Parquet.R

--- Read Parquet.R
+++
@@ -1,6 +0,0 @@
-#install.packages("arrow")
-
-library(arrow)
-path = "path" #Set path of Parquet File downloaded from Github repo: https://github.com/lucasweyrich958/DATA622/blob/main/sales.parquet
-
-sales = read_parquet(path)
